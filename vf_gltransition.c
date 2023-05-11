9aead86cab5021681a52d35003d776d199f4e2cd
chenyexin2012 <chenyexin2012@126.com>
2023-05-11 19:30:37 +0800

    modify

--- vf_gltransition.c
+++ vf_gltransition.c
@@ -8,9 +8,9 @@
 #include "internal.h"
 #include "framesync.h"
 
-#ifndef __APPLE__
-# define GL_TRANSITION_USING_EGL //remove this line if you don't want to use EGL
-#endif
+//#ifndef __APPLE__
+//# define GL_TRANSITION_USING_EGL //remove this line if you don't want to use EGL
+//#endif
 
 #ifdef __APPLE__
 # define __gl_h_
@@ -494,6 +494,7 @@ static av_cold void uninit(AVFilterContext *ctx) {
 static int query_formats(AVFilterContext *ctx)
 {
   static const enum AVPixelFormat formats[] = {
+    //AV_PIX_FMT_YUV420P,
     AV_PIX_FMT_RGB24,
     AV_PIX_FMT_NONE
   };
@@ -551,7 +552,6 @@ static const AVFilterPad gltransition_inputs[] = {
     .name = "to",
     .type = AVMEDIA_TYPE_VIDEO,
   },
-  {NULL}
 };
 
 static const AVFilterPad gltransition_outputs[] = {
@@ -560,7 +560,6 @@ static const AVFilterPad gltransition_outputs[] = {
     .type = AVMEDIA_TYPE_VIDEO,
     .config_props = config_output,
   },
-  {NULL}
 };
 
 AVFilter ff_vf_gltransition = {
@@ -570,10 +569,13 @@ AVFilter ff_vf_gltransition = {
   .preinit       = gltransition_framesync_preinit,
   .init          = init,
   .uninit        = uninit,
-  .query_formats = query_formats,
+ // .query_formats = query_formats,
+  FILTER_QUERY_FUNC(query_formats),
   .activate      = activate,
-  .inputs        = gltransition_inputs,
-  .outputs       = gltransition_outputs,
+ // .inputs        = gltransition_inputs,
+  FILTER_INPUTS(gltransition_inputs),
+ // .outputs       = gltransition_outputs,
+  FILTER_OUTPUTS(gltransition_outputs),
   .priv_class    = &gltransition_class,
   .flags         = AVFILTER_FLAG_SUPPORT_TIMELINE_GENERIC
 };
